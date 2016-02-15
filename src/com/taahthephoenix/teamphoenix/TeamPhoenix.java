9122a90ab552560002c56b111a9da639b6c18299
ThePheonixBlader <thepheonixblader@gmail.com>
2016-02-14 19:31:37 -0800

    1st Commit

---
+++ src/com/taahthephoenix/teamphoenix/TeamPhoenix.java
@@ -0,0 +1,46 @@
+package com.taahthephoenix.teamphoenix;
+
+import net.pravian.bukkitlib.BukkitLib;
+import net.pravian.bukkitlib.command.BukkitCommandHandler;
+import net.pravian.bukkitlib.config.YamlConfig;
+import net.pravian.bukkitlib.implementation.BukkitPlugin;
+import net.pravian.bukkitlib.util.LoggerUtils;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class TeamPhoenix extends BukkitPlugin {
+    
+    public TeamPhoenix plugin;
+    public BukkitCommandHandler handler;
+    public YamlConfig config;
+    
+    @Override
+    public void onLoad() {
+        this.plugin = this;
+        this.handler = new BukkitCommandHandler(plugin);
+        
+    }
+    
+    @Override
+    public void onEnable() {
+        BukkitLib.init(plugin);
+        config = new YamlConfig(plugin, "config.yml", true);
+        config.load();
+        LoggerUtils.info(plugin, "Developed by " + plugin.getAuthors() + " version: " + plugin.getVersion() + "enabled!");
+        
+    }
+    
+    @Override
+    public void onDisable(){
+        config.save();
+        LoggerUtils.info(plugin, "Developed by " + plugin.getAuthors() + " version: " + plugin.getVersion() + "disabled!");
+        
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        return handler.handleCommand(sender, cmd, commandLabel, args);
+    }
+
+    
+}
